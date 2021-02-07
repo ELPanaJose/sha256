a01f7ed86ef54f7889751256c5b63f5eaf651a4e
pai <69026987+ELPanaJose@users.noreply.github.com>
2021-02-07 13:19:40 -0500

    Update sha256.js

--- sha256.js
+++ sha256.js
@@ -1,9 +1,7 @@
-const prompt = require("prompt-sync")({ sigint: true });
+const prompt = require("prompt-sync")();
 const figlet = require("figlet");
 const chalk = require("chalk");
 const crypto = require("crypto");
-
-const encrypter = crypto.createHmac("sha256", "").update("").digest("hex");
 
 console.log(
   chalk.greenBright(
@@ -11,15 +9,17 @@ console.log(
       font: "ANSI Shadow",
       horizontalLayout: "default",
       verticalLayout: "default",
-      width: 100,
+      width: 80,
       whitespaceBreak: true,
     })
   )
 );
 console.log(
-  chalk.blueBright("[*] this is a tool to encrypt to a 64 characters hash!")
+  chalk.blueBright("[!] this is a tool to encrypt to a 32 characters hash!")
 );
 
 let rawI = prompt(`[?] what do you want to encrypt? : `);
+// the constant of the encriptation
+const encrypter = crypto.createHmac("sha256", "").update(rawI).digest("hex");
 
 console.log((chalk.cyan)`[*] your hash: ${encrypter}`);
